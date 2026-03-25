feeabf0774fa7271cd7ff0163c50874e9f1c6175
jullsv <usavenkova15@mail.ru>
2026-03-25 16:15:27 +0700

    Update lab04_.py

--- lab04/lab04_.py
+++ lab04/lab04_.py
@@ -1,14 +1,13 @@
 import random
 
-
-class BasicGenerator:
-    def __init__(self, betta, m, seed):
-        self.betta = betta
+class BaseGenerator:
+    def __init__(self, b, m, seed):
+        self.b = b
         self.m = m
         self.x = seed
 
     def rand(self):
-        self.x = (self.betta * self.x) % self.m
+        self.x = (self.b * self.x) % self.m
         return self.x / self.m
 
     def generate(self, n):
@@ -18,64 +17,58 @@ class BasicGenerator:
         return numbers
 
 
-def calculate(numbers):
+def count(numbers):
     n = len(numbers)
-
     mean = sum(numbers) / n
-
     var = 0
     for x in numbers:
         var += (x - mean) ** 2
     var = var / n
-
     return mean, var
 
 
 def main():
-    betta = 16807
-    m = 2**32
-    seed = 1
+    b = 2**32 + 3
+    m = 2**63
+    seed = b
 
     N = 100_000
+    t_mean = 0.5
+    t_var = 1.0 / 12.0
 
-    theor_mean = 0.5
-    theor_var = 1.0 / 12.0
-
-    # --- Базовый минимум ---
-    generator = BasicGenerator(betta, m, seed)
+    generator = BaseGenerator(b, m, seed)
     numbers = generator.generate(N)
-    mean, var = calculate(numbers)
+    mean, var = count(numbers)
 
-    # --- Встроенный генератор ---
     numbers_ = []
-
     for i in range(N):
         numbers_.append(random.random())
 
-    mean_, var_ = calculate(numbers_)
+    mean_, var_ = count(numbers_)
+    diff_mean = mean - t_mean
+    diff_var = var - t_var
+    
+    diff_mean_ = mean_ - t_mean
+    diff_var_ = var_ - t_var
 
-    diff_mean = mean - theor_mean
-    diff_var = var - theor_var
-
-    diff_mean_ = mean_ - theor_mean
-    diff_var_ = var_ - theor_var
-
-    # --- Вывод ---
     print("Базовый датчик")
-    print(f"Среднее: {mean:.6f} (diff = {diff_mean:+.6e})")
-    print(f"Дисперсия: {var:.6f} (diff = {diff_var:+.6e})")
+    print()
+    print(f"Среднее: {mean:.6f} (отклонение = {diff_mean:+.6e})")
+    print(f"Дисперсия: {var:.6f} (отклонение = {diff_var:+.6e})")
 
     print()
 
-    print("Встроенный генератор")
-    print(f"Среднее: {mean_:.6f} (diff = {diff_mean_:+.6e})")
-    print(f"Дисперсия: {var_:.6f} (diff = {diff_var_:+.6e})")
+    print("Встроенный датчик")
+    print()
+    print(f"Среднее: {mean_:.6f} (отклонение = {diff_mean_:+.6e})")
+    print(f"Дисперсия: {var_:.6f} (отклонение = {diff_var_:+.6e})")
 
     print()
 
     print("Теоретические значения")
-    print(f"Среднее: {theor_mean:.6f}")
-    print(f"Дисперсия: {theor_var:.6f}")
+    print()
+    print(f"Среднее: {t_mean:.6f}")
+    print(f"Дисперсия: {t_var:.6f}")
 
 
 if __name__ == "__main__":
